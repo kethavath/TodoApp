e2330d2e2bf7ac59bc7651330ba2a20081550e65
Thukaram Kethavath <thukaramkethavath@thukarams-mbp.fios-router.home>
2017-05-24 21:55:05 -0400

    App to create list

--- TodoApp/FirstViewController.swift
+++ TodoApp/FirstViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
+class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    var items:[String] = []
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count
+    }
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+        
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+       override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    override func viewDidAppear(_ Animated:Bool){
+            let userObject = UserDefaults.standard.object(forKey: "items")
+            if let tempItems = userObject as? [String]
+            {
+                    items = tempItems
+            }
+ 
+            table.reloadData()
+        
     }
-
-
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if  editingStyle == UITableViewCellEditingStyle.delete{
+            
+            items.remove(at: indexPath.row)
+            table.reloadData()
+            
+    UserDefaults.standard.set(items,forKey: "items")
+            
+        }
+        
+        
+    }
+    
 }
 
--- TodoApp/SecondViewController.swift
+++ TodoApp/SecondViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    var items:NSMutableArray = []
+    
+    @IBOutlet var userText: UITextField!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+  let userObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        if let tempItems = userObject as? [String]
+        {
+            items = tempItems
+            items.append(userText.text!)
+        }
+        else{
+            
+        items = [userText.text!]
+        }
+        UserDefaults.standard.set(items,forKey: "items")
+        
+        userText.text = ""
+        }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+    return true
+    }
+   
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
